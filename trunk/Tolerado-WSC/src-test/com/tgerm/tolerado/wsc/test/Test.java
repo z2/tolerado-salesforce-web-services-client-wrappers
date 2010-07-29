5057b965235503dd672de6fcaafcdd49d2d05990
abhinavgupta697 <abhinavgupta697@73932599-89ed-8a20-aa45-8ea7ebf36a09>
2010-07-29 14:06:35 +0000

    Initial import

---
+++ trunk/Tolerado-WSC/src-test/com/tgerm/tolerado/wsc/test/Test.java
@@ -0,0 +1,45 @@
+/*
+Copyright (c) 2010 tgerm.com
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the author may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, 
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.tgerm.tolerado.wsc.test;
+
+import junit.framework.TestCase;
+
+import com.sforce.soap.partner.QueryResult;
+import com.tgerm.tolerado.samples.cfg.LoginCfg;
+import com.tgerm.tolerado.wsc.core.Credential;
+import com.tgerm.tolerado.wsc.partner.ToleradoPartnerStub;
+
+public class Test extends TestCase {
+
+	public void testLogin() throws Exception {
+		Credential credential = LoginCfg.self.getCredential();
+		ToleradoPartnerStub s = new ToleradoPartnerStub(credential);
+		QueryResult qr = s.query("select id from contact limit 1");
+	}
+}
